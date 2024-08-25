4654c77b9176a69d90eb13cb28124d7cb8d3de52
AguilaAzulcrema <92063998+AguilaAzulcrema@users.noreply.github.com>
2024-08-24 19:18:02 -0600

    Update script.js

--- json2/script.js
+++ json2/script.js
@@ -1,42 +1,41 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://aguilaazulcrema.github.io/json2/events.json')
+    fetch('events.json') // Asegúrate de que la ruta al archivo JSON sea correcta
         .then(response => response.json())
         .then(data => {
-            const eventosDiv = document.querySelector('.eventos');
-            data.events.forEach(event => {
-                const eventHTML = `
-                <div class='event'>
-                    <div class='event2'>
-                        <p class="pais">${event.competition}</p>
-                    </div>
-                    <div class='kotak'>
-                        <div class='left'>
-                            <img class='logo1' src='${event.teamLeft.logo}'/>
-                            <div class='team'>${event.teamLeft.name}</div>
-                        </div>
-                        <div class='center'>
-                            <div class='date1' data-target-date='${event.date}'>${new Date(event.date).toLocaleDateString('es-ES')}</div>
-                            <div class='wak'>${event.time}</div>
-                            <div class='jam jam1' data-target-time='${event.time}'></div>
-                        </div>
-                        <div class='right'>
-                            <img class='logo1' src='${event.teamRight.logo}'/>
-                            <div class='team'>${event.teamRight.name}</div>
-                        </div>
-                    </div>
-                    <div class='buttons-container'>
-                        <div class='buttons-row'>
-                            ${event.streamOptions.map(option => `<a class='button' data-stream='${option}'>Opcion</a>`).join('')}
-                        </div>
-                    </div>
-                </div>`;
-                
-                eventosDiv.insertAdjacentHTML('beforeend', eventHTML);
-            });
+            const events = data.events;
+            const container = document.querySelector('.container'); // Asegúrate de que este selector sea correcto
             
-            // Aquí puedes llamar a tu función de ajuste de horario si es necesario
-            ajustarHorarios();
+            events.forEach(event => {
+                // Crear HTML para cada evento
+                const eventHTML = `
+                    <div class="event">
+                        <div class="event2">
+                            <p class="pais">Premier League</p>
+                        </div>
+                        <div class="kotak">
+                            <div class="left">
+                                <div class="logo ${event.team1.logo}"></div>
+                                <div class="team">${event.team1.name}</div>
+                            </div>
+                            <div class="center">
+                                <div class="date1" data-target-date="${event.date}">${event.date}</div>
+                                <div class="wak">${event.time}</div>
+                                <div class="jam jam1" data-target-time="${event.time}"></div>
+                            </div>
+                            <div class="right">
+                                <div class="logo ${event.team2.logo}"></div>
+                                <div class="team">${event.team2.name}</div>
+                            </div>
+                        </div>
+                        <div class="buttons-container">
+                            <div class="buttons-row">
+                                ${event.streams.map(stream => `<a class="button" data-stream="${stream}">CH ${event.streams.indexOf(stream) + 1}</a>`).join('')}
+                            </div>
+                        </div>
+                    </div>
+                `;
+                container.innerHTML += eventHTML;
+            });
         })
-        .catch(error => console.error('Error cargando el JSON:', error));
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
 });
