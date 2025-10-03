16ecd8da353237cde65406d55a03d9192bbbe80d
Luis Olv <soldadokallejero462@gmail.com>
2025-10-03 13:13:25 -0600

    update match

--- matchpro/script.js
+++ matchpro/script.js
@@ -185,7 +185,7 @@ function cargarLogos() {
     const logo2 = new Image();
     
     // Lista de fondos que usarán medidas especiales
-    const fondosConMedidasEspeciales = ['fondo5', 'fondorosa', 'ligamx femenil new', 'ligamx new', 'ligadeexpansion new', 'laliga new', 'premierleague new', 'serie a new', 'serie a 2 new'];
+    const fondosConMedidasEspeciales = ['fondo5', 'fondorosa'];
     
     // Buscar las rutas de los logos teniendo en cuenta ligas compuestas
     if (ligaSeleccionada === 'todos') {
